6a78d328719da82486ef8b9c88d3a041a1710c61
mab6887 <mab6887@thi.de>
2024-11-28 22:55:13 +0100

    instantiation entitys (Zeitslots incomplete)

---
+++ TerminvereinbarungApplication/Program.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TerminvereinbarungLib;
+
+namespace TerminvereinbarungApplication
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+
+            //Instanziierung der Behandlungen
+
+            Behandlung ultraschall = new Behandlung();
+            ultraschall.Behandlungart = "Ultraschall";
+            ultraschall.Behandlungsdauer = new TimeSpan(0, 30, 0);
+
+            Behandlung impfung = new Behandlung();
+            impfung.Behandlungart = "Impfung";
+            impfung.Behandlungsdauer = new TimeSpan(0, 15, 0);
+
+            Behandlung blutabnahme = new Behandlung();
+            blutabnahme.Behandlungart = "Blutabnahme";
+            blutabnahme.Behandlungsdauer = new TimeSpan(0, 10, 0);
+
+            Behandlung röntgen = new Behandlung();
+            röntgen.Behandlungart = "Röntgen";
+            röntgen.Behandlungsdauer = new TimeSpan(0, 20, 0);
+
+            Behandlung diagnostischesGespräch = new Behandlung();
+            diagnostischesGespräch.Behandlungart = "Diagnostisches Gespräch";
+            diagnostischesGespräch.Behandlungsdauer = new TimeSpan(0, 30, 0);
+
+            Behandlung akupunktur = new Behandlung();
+            akupunktur.Behandlungart = "Akupunktur";
+            akupunktur.Behandlungsdauer = new TimeSpan(0, 45, 0);
+
+            Behandlung verbinden = new Behandlung();
+            verbinden.Behandlungart = "Verbinden";
+            verbinden.Behandlungsdauer = new TimeSpan(0, 15, 0);
+
+            Behandlung elektrokardiogramm = new Behandlung();
+            elektrokardiogramm.Behandlungart = "Elektrokardiogramm";
+            elektrokardiogramm.Behandlungsdauer = new TimeSpan(0, 20, 0);
+
+            Behandlung opration = new Behandlung();
+            opration.Behandlungart = "Operation";
+            opration.Behandlungsdauer = new TimeSpan(1, 0, 0);
+
+
+            //Instanziierung der UserÄrzte
+
+            User drSchlingenbein = new User();
+            drSchlingenbein.Vorname = "Jens";
+            drSchlingenbein.Nachname = "Dr. Schlingenbein";
+            drSchlingenbein.Geburtsdatum = "03.10.1970";
+            drSchlingenbein.Telefon = "0176 1737639";
+            drSchlingenbein.EMail = "jens.schlingenbein@gmail.com";
+            drSchlingenbein.Arzt = true;
+            drSchlingenbein.Admin = false;
+            drSchlingenbein.Passwort = "Sonne1";
+
+            User drLeiserbach = new User();
+            drLeiserbach.Vorname = "Carola";
+            drLeiserbach.Nachname = "Dr. Leiserbach";
+            drLeiserbach.Geburtsdatum = "12.12.1980";
+            drLeiserbach.Telefon = "0146 1737639";
+            drLeiserbach.EMail = "crola.leiserbach@gmail.com";
+            drLeiserbach.Arzt = true;
+            drLeiserbach.Admin = false;
+            drLeiserbach.Passwort = "Susanne#";
+
+            User drFreud = new User();
+            drFreud.Vorname = "Sigmund";
+            drFreud.Nachname = "Dr. Freud";
+            drFreud.Geburtsdatum = "06.05.1856";
+            drFreud.Telefon = "0176 5027639";
+            drFreud.EMail = "sigmund.freud@gmail.com";
+            drFreud.Arzt = true;
+            drFreud.Admin = false;
+            drFreud.Passwort = "Psycho1";
+
+            User drSchönberger = new User();
+            drSchönberger.Vorname = "Thomas";
+
+            drSchönberger.Nachname = "Dr. Schönberger";
+            drSchönberger.Geburtsdatum = "03.10.1975";
+            drSchönberger.Telefon = "0156 1734269";
+            drSchönberger.EMail = "thomas.schoenberger@gmail.com";
+            drSchönberger.Arzt = true;
+            drSchönberger.Admin = false;
+            drSchönberger.Passwort = "SchönergroßerBergg";
+
+            User drMaxwell = new User();
+            drMaxwell.Vorname = "Hamilton";
+            drMaxwell.Nachname = "Dr. Maxwell";
+            drMaxwell.Geburtsdatum = "07.08.1985";
+            drMaxwell.Telefon = "0122 1749639";
+            drMaxwell.EMail = "hamilton.maxwell@gmail.com";
+            drMaxwell.Arzt = true;
+            drMaxwell.Admin = false;
+            drMaxwell.Passwort = "CircusMa#imus";
+
+            User drKocht = new User();
+            drKocht.Vorname = "Robert";
+            drKocht.Nachname = "Dr. Kocht";
+            drKocht.Geburtsdatum = "03.10.1968";
+            drKocht.Telefon = "0176 6589339";
+            drKocht.EMail = "robert.kocht@gmail.com";
+            drKocht.Arzt = true;
+            drKocht.Admin = false;
+            drKocht.Passwort = "Kochtopf1";
+
+            User drPlankton = new User();
+            drPlankton.Vorname = "Max";
+            drPlankton.Nachname = "Dr. Plankton";
+            drPlankton.Geburtsdatum = "23.06.1968";
+            drPlankton.Telefon = "01520 174t62339";
+            drPlankton.EMail = "max.plankton@gmail.com";
+            drPlankton.Arzt = true;
+            drPlankton.Admin = false;
+            drPlankton.Passwort = "Sp0ng3B00B";
+
+
+            //Instanziierung der UserAdmin
+
+            User polo = new User();
+
+            polo.Vorname = "Marco";
+            polo.Nachname = "Polo";
+            polo.Geburtsdatum = "05.04.2002";
+            polo.Telefon = "0176 12345678";
+            polo.EMail = "mab6887@thi.de";
+            polo.Arzt = false;
+            polo.Admin = true;
+            polo.Passwort = "1234";
+            
+
+            //Instanziierung der UserPatienten
+
+            User lovelace = new User();
+            lovelace.Vorname = "Ada";
+            lovelace.Nachname = "Lovelace";
+            lovelace.Geburtsdatum = "10.12.1815";
+            lovelace.Telefon = "015 35792493";
+            lovelace.EMail = "ada.lovelace@icloud.com";
+            lovelace.Krankenkasse = "AOK";
+            lovelace.Arzt = false;
+            lovelace.Admin = false;
+            lovelace.Passwort = "AdamandEva";
+
+            User bubble = new User();
+            bubble.Vorname = "Edwin";
+            bubble.Nachname = "Bubble";
+            bubble.Geburtsdatum = "10.12.1815";
+            bubble.Telefon = "015 35792493";
+            bubble.EMail = "edwin.bubble@gmx.de";
+            bubble.Krankenkasse = "DAK";
+            bubble.Arzt = false;
+            bubble.Admin = false;
+            bubble.Passwort = "Bubblegum";
+
+            User deckenbauer = new User();
+            deckenbauer.Vorname = "Franz";
+            deckenbauer.Nachname = "Deckenbauer";
+            deckenbauer.Geburtsdatum = "10.12.1960";
+            deckenbauer.Telefon = "0174 8402ß4582";
+            deckenbauer.EMail = "franz.deckenbauer@gmail.com";
+            deckenbauer.Krankenkasse = "TK";
+            deckenbauer.Arzt = false;
+            deckenbauer.Admin = false;
+            deckenbauer.Passwort = "Dachdecker";
+
+            User hopper = new User();
+            hopper.Vorname = "Grace";
+            hopper.Nachname = "Hopper";
+            hopper.Geburtsdatum = "09.12.1906";
+            hopper.Telefon = "0176 12348648";
+            hopper.EMail = "grace.hopper@hotmail.com";
+            hopper.Krankenkasse = "BKK";
+            hopper.Arzt = false;
+            hopper.Admin = false;
+            hopper.Passwort = "GrasHopper";
+
+            User hamilton = new User();
+            hamilton.Vorname = "Margaret";
+            hamilton.Nachname = "Hamilton";
+            hamilton.Geburtsdatum = "17.08.1936";
+            hamilton.Telefon = "0176 53748648";
+            hamilton.EMail = "margarez.hamilton.web.de";
+            hamilton.Krankenkasse = "Barmer";
+            hamilton.Arzt = false;
+            hamilton.Admin = false;
+            hamilton.Passwort = "HamiltonsRule";
+
+            User perlman = new User();
+            perlman.Vorname = "Radia";
+            perlman.Nachname = "Perlman";
+            perlman.Geburtsdatum = "01.01.1951";
+            perlman.Telefon = "0176 12348648";
+            perlman.EMail = "radia.perlman@gmail.com";
+            perlman.Krankenkasse = "IKK";
+            perlman.Arzt = false;
+            perlman.Admin = false;
+            perlman.Passwort = "PearlBracelet";
+
+
+            //Instanziierung der Zeitslots
+
+
+            //Instanziierung eines Tages möglicherweise später mit Mittagspause und 30 min Slots weitere for schleife für woche, weitere for schleife für jahr
+           
+            for (int i = 0; i < 8; i++)
+            {
+                Zeitslot slot = new Zeitslot();
+                slot.Startzeitpunkt = new DateTime(2025, 1, 1, 8 + i, 0, 0);
+                slot.Dauer = new TimeSpan(1, 0, 0);
+            }
+            
+
+
+
+
+        }
+    }
+}
--- TerminvereinbarungLib/TerminvereinbarungModel.edmx.sql
+++ TerminvereinbarungLib/TerminvereinbarungModel.edmx.sql
@@ -2,7 +2,7 @@
 -- --------------------------------------------------
 -- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
 -- --------------------------------------------------
--- Date Created: 11/26/2024 12:48:46
+-- Date Created: 11/28/2024 17:53:56
 -- Generated from EDMX file: C:\Users\marco_p\source\repos\Terminvereinbarung\TerminvereinbarungLib\TerminvereinbarungModel.edmx
 -- --------------------------------------------------
 
@@ -17,11 +17,53 @@ GO
 -- Dropping existing FOREIGN KEY constraints
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[FK_Arzt_User]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Arzt] DROP CONSTRAINT [FK_Arzt_User];
+GO
+IF OBJECT_ID(N'[dbo].[FK_Arzt_Behandlung]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[Arzt] DROP CONSTRAINT [FK_Arzt_Behandlung];
+GO
+IF OBJECT_ID(N'[dbo].[FK_ArztTermin]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[TerminSet] DROP CONSTRAINT [FK_ArztTermin];
+GO
+IF OBJECT_ID(N'[dbo].[FK_PatientTermin]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[TerminSet] DROP CONSTRAINT [FK_PatientTermin];
+GO
+IF OBJECT_ID(N'[dbo].[FK_BehandlungTermin]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[TerminSet] DROP CONSTRAINT [FK_BehandlungTermin];
+GO
+IF OBJECT_ID(N'[dbo].[FK_TerminZeitslot]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[TerminSet] DROP CONSTRAINT [FK_TerminZeitslot];
+GO
+IF OBJECT_ID(N'[dbo].[FK_ArztZeitslot_User]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[ArztZeitslot] DROP CONSTRAINT [FK_ArztZeitslot_User];
+GO
+IF OBJECT_ID(N'[dbo].[FK_ArztZeitslot_Zeitslot]', 'F') IS NOT NULL
+    ALTER TABLE [dbo].[ArztZeitslot] DROP CONSTRAINT [FK_ArztZeitslot_Zeitslot];
+GO
 
 -- --------------------------------------------------
 -- Dropping existing tables
 -- --------------------------------------------------
 
+IF OBJECT_ID(N'[dbo].[UserSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[UserSet];
+GO
+IF OBJECT_ID(N'[dbo].[BehandlungSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[BehandlungSet];
+GO
+IF OBJECT_ID(N'[dbo].[TerminSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[TerminSet];
+GO
+IF OBJECT_ID(N'[dbo].[ZeitslotSet]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[ZeitslotSet];
+GO
+IF OBJECT_ID(N'[dbo].[Arzt]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[Arzt];
+GO
+IF OBJECT_ID(N'[dbo].[ArztZeitslot]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[ArztZeitslot];
+GO
 
 -- --------------------------------------------------
 -- Creating all tables
@@ -35,78 +77,53 @@ CREATE TABLE [dbo].[UserSet] (
     [Geburtsdatum] nvarchar(max)  NOT NULL,
     [Telefon] nvarchar(max)  NOT NULL,
     [EMail] nvarchar(max)  NOT NULL,
-    [Krankenkasse] nvarchar(max)  NOT NULL,
+    [Krankenkasse] nvarchar(max)  NULL,
+    [Arzt] bit  NOT NULL,
     [Admin] bit  NOT NULL,
-    [Arzt] bit  NOT NULL
+    [Passwort] nvarchar(max)  NOT NULL
 );
 GO
 
 -- Creating table 'BehandlungSet'
 CREATE TABLE [dbo].[BehandlungSet] (
     [Id] int IDENTITY(1,1) NOT NULL,
-    [Behandlungsart] nvarchar(max)  NOT NULL,
-    [Dauer] smallint  NOT NULL,
-    [behandelnderArzt] nvarchar(max)  NOT NULL
+    [Behandlungart] nvarchar(max)  NOT NULL,
+    [Behandlungsdauer] time  NOT NULL
 );
 GO
 
 -- Creating table 'TerminSet'
 CREATE TABLE [dbo].[TerminSet] (
     [Id] int IDENTITY(1,1) NOT NULL,
+    [ArztId] int  NOT NULL,
+    [PatientId] int  NOT NULL,
     [angefragt] bit  NOT NULL,
     [bestätigt] bit  NOT NULL,
-    [abgesagt] bit  NOT NULL,
     [abgeschlossen] bit  NOT NULL,
-    [verfügbarerArzt] nvarchar(max)  NOT NULL
+    [BehandlungId] int  NOT NULL,
+    [ZeitslotId] int  NOT NULL
 );
 GO
 
--- Creating table 'ArztBehandlung'
-CREATE TABLE [dbo].[ArztBehandlung] (
-    [wirdangeboten_Id] int  NOT NULL,
-    [bietetan_Id] int  NOT NULL
+-- Creating table 'ZeitslotSet'
+CREATE TABLE [dbo].[ZeitslotSet] (
+    [Id] int IDENTITY(1,1) NOT NULL,
+    [Startzeitpunkt] datetime  NOT NULL,
+    [Dauer] time  NOT NULL
 );
 GO
 
--- Creating table 'ArztTermin'
-CREATE TABLE [dbo].[ArztTermin] (
-    [mitArztverfügbar_Id] int  NOT NULL,
-    [freizu_Id] int  NOT NULL
+-- Creating table 'Arzt'
+CREATE TABLE [dbo].[Arzt] (
+    [qualifizierteÄrzte_Id] int  NOT NULL,
+    [Behandlungungen_Id] int  NOT NULL
 );
 GO
 
--- Creating table 'UserTerminBestätigung'
-CREATE TABLE [dbo].[UserTerminBestätigung] (
-    [wirdbestätigt_Id] int  NOT NULL,
-    [bestätigt_Id] int  NOT NULL
-);
-GO
-
--- Creating table 'UserBehandlung'
-CREATE TABLE [dbo].[UserBehandlung] (
-    [wirdgewählt_Id] int  NOT NULL,
-    [wähltBehandlung_Id] int  NOT NULL
-);
-GO
-
--- Creating table 'UserTermin'
-CREATE TABLE [dbo].[UserTermin] (
-    [wirdgewählt_Id] int  NOT NULL,
-    [wähltTermin_Id] int  NOT NULL
-);
-GO
-
--- Creating table 'UserTermin1'
-CREATE TABLE [dbo].[UserTermin1] (
-    [CUD_T_Id] int  NOT NULL,
-    [CUDT_Id] int  NOT NULL
-);
-GO
-
--- Creating table 'UserBehandlung1'
-CREATE TABLE [dbo].[UserBehandlung1] (
-    [CUD_B_Id] int  NOT NULL,
-    [CUDB_Id] int  NOT NULL
+-- Creating table 'ArztZeitslot'
+CREATE TABLE [dbo].[ArztZeitslot] (
+    [verfügbareÄrzte_Id] int  NOT NULL,
+    [ZeitslotArzt_Id] int  NOT NULL
 );
 GO
 
@@ -132,218 +149,134 @@ ADD CONSTRAINT [PK_TerminSet]
     PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
--- Creating primary key on [wirdangeboten_Id], [bietetan_Id] in table 'ArztBehandlung'
-ALTER TABLE [dbo].[ArztBehandlung]
-ADD CONSTRAINT [PK_ArztBehandlung]
-    PRIMARY KEY CLUSTERED ([wirdangeboten_Id], [bietetan_Id] ASC);
+-- Creating primary key on [Id] in table 'ZeitslotSet'
+ALTER TABLE [dbo].[ZeitslotSet]
+ADD CONSTRAINT [PK_ZeitslotSet]
+    PRIMARY KEY CLUSTERED ([Id] ASC);
 GO
 
--- Creating primary key on [mitArztverfügbar_Id], [freizu_Id] in table 'ArztTermin'
-ALTER TABLE [dbo].[ArztTermin]
-ADD CONSTRAINT [PK_ArztTermin]
-    PRIMARY KEY CLUSTERED ([mitArztverfügbar_Id], [freizu_Id] ASC);
+-- Creating primary key on [qualifizierteÄrzte_Id], [Behandlungungen_Id] in table 'Arzt'
+ALTER TABLE [dbo].[Arzt]
+ADD CONSTRAINT [PK_Arzt]
+    PRIMARY KEY CLUSTERED ([qualifizierteÄrzte_Id], [Behandlungungen_Id] ASC);
 GO
 
--- Creating primary key on [wirdbestätigt_Id], [bestätigt_Id] in table 'UserTerminBestätigung'
-ALTER TABLE [dbo].[UserTerminBestätigung]
-ADD CONSTRAINT [PK_UserTerminBestätigung]
-    PRIMARY KEY CLUSTERED ([wirdbestätigt_Id], [bestätigt_Id] ASC);
-GO
-
--- Creating primary key on [wirdgewählt_Id], [wähltBehandlung_Id] in table 'UserBehandlung'
-ALTER TABLE [dbo].[UserBehandlung]
-ADD CONSTRAINT [PK_UserBehandlung]
-    PRIMARY KEY CLUSTERED ([wirdgewählt_Id], [wähltBehandlung_Id] ASC);
-GO
-
--- Creating primary key on [wirdgewählt_Id], [wähltTermin_Id] in table 'UserTermin'
-ALTER TABLE [dbo].[UserTermin]
-ADD CONSTRAINT [PK_UserTermin]
-    PRIMARY KEY CLUSTERED ([wirdgewählt_Id], [wähltTermin_Id] ASC);
-GO
-
--- Creating primary key on [CUD_T_Id], [CUDT_Id] in table 'UserTermin1'
-ALTER TABLE [dbo].[UserTermin1]
-ADD CONSTRAINT [PK_UserTermin1]
-    PRIMARY KEY CLUSTERED ([CUD_T_Id], [CUDT_Id] ASC);
-GO
-
--- Creating primary key on [CUD_B_Id], [CUDB_Id] in table 'UserBehandlung1'
-ALTER TABLE [dbo].[UserBehandlung1]
-ADD CONSTRAINT [PK_UserBehandlung1]
-    PRIMARY KEY CLUSTERED ([CUD_B_Id], [CUDB_Id] ASC);
+-- Creating primary key on [verfügbareÄrzte_Id], [ZeitslotArzt_Id] in table 'ArztZeitslot'
+ALTER TABLE [dbo].[ArztZeitslot]
+ADD CONSTRAINT [PK_ArztZeitslot]
+    PRIMARY KEY CLUSTERED ([verfügbareÄrzte_Id], [ZeitslotArzt_Id] ASC);
 GO
 
 -- --------------------------------------------------
 -- Creating all FOREIGN KEY constraints
 -- --------------------------------------------------
 
--- Creating foreign key on [wirdangeboten_Id] in table 'ArztBehandlung'
-ALTER TABLE [dbo].[ArztBehandlung]
-ADD CONSTRAINT [FK_ArztBehandlung_User]
-    FOREIGN KEY ([wirdangeboten_Id])
+-- Creating foreign key on [qualifizierteÄrzte_Id] in table 'Arzt'
+ALTER TABLE [dbo].[Arzt]
+ADD CONSTRAINT [FK_Arzt_User]
+    FOREIGN KEY ([qualifizierteÄrzte_Id])
     REFERENCES [dbo].[UserSet]
         ([Id])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating foreign key on [bietetan_Id] in table 'ArztBehandlung'
-ALTER TABLE [dbo].[ArztBehandlung]
-ADD CONSTRAINT [FK_ArztBehandlung_Behandlung]
-    FOREIGN KEY ([bietetan_Id])
+-- Creating foreign key on [Behandlungungen_Id] in table 'Arzt'
+ALTER TABLE [dbo].[Arzt]
+ADD CONSTRAINT [FK_Arzt_Behandlung]
+    FOREIGN KEY ([Behandlungungen_Id])
     REFERENCES [dbo].[BehandlungSet]
         ([Id])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating non-clustered index for FOREIGN KEY 'FK_ArztBehandlung_Behandlung'
-CREATE INDEX [IX_FK_ArztBehandlung_Behandlung]
-ON [dbo].[ArztBehandlung]
-    ([bietetan_Id]);
+-- Creating non-clustered index for FOREIGN KEY 'FK_Arzt_Behandlung'
+CREATE INDEX [IX_FK_Arzt_Behandlung]
+ON [dbo].[Arzt]
+    ([Behandlungungen_Id]);
 GO
 
--- Creating foreign key on [mitArztverfügbar_Id] in table 'ArztTermin'
-ALTER TABLE [dbo].[ArztTermin]
-ADD CONSTRAINT [FK_ArztTermin_User]
-    FOREIGN KEY ([mitArztverfügbar_Id])
+-- Creating foreign key on [ArztId] in table 'TerminSet'
+ALTER TABLE [dbo].[TerminSet]
+ADD CONSTRAINT [FK_ArztTermin]
+    FOREIGN KEY ([ArztId])
     REFERENCES [dbo].[UserSet]
         ([Id])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating foreign key on [freizu_Id] in table 'ArztTermin'
-ALTER TABLE [dbo].[ArztTermin]
-ADD CONSTRAINT [FK_ArztTermin_Termin]
-    FOREIGN KEY ([freizu_Id])
-    REFERENCES [dbo].[TerminSet]
-        ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- Creating non-clustered index for FOREIGN KEY 'FK_ArztTermin'
+CREATE INDEX [IX_FK_ArztTermin]
+ON [dbo].[TerminSet]
+    ([ArztId]);
 GO
 
--- Creating non-clustered index for FOREIGN KEY 'FK_ArztTermin_Termin'
-CREATE INDEX [IX_FK_ArztTermin_Termin]
-ON [dbo].[ArztTermin]
-    ([freizu_Id]);
-GO
-
--- Creating foreign key on [wirdbestätigt_Id] in table 'UserTerminBestätigung'
-ALTER TABLE [dbo].[UserTerminBestätigung]
-ADD CONSTRAINT [FK_UserTerminBestätigung_User]
-    FOREIGN KEY ([wirdbestätigt_Id])
+-- Creating foreign key on [PatientId] in table 'TerminSet'
+ALTER TABLE [dbo].[TerminSet]
+ADD CONSTRAINT [FK_PatientTermin]
+    FOREIGN KEY ([PatientId])
     REFERENCES [dbo].[UserSet]
         ([Id])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating foreign key on [bestätigt_Id] in table 'UserTerminBestätigung'
-ALTER TABLE [dbo].[UserTerminBestätigung]
-ADD CONSTRAINT [FK_UserTerminBestätigung_Termin]
-    FOREIGN KEY ([bestätigt_Id])
-    REFERENCES [dbo].[TerminSet]
-        ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- Creating non-clustered index for FOREIGN KEY 'FK_PatientTermin'
+CREATE INDEX [IX_FK_PatientTermin]
+ON [dbo].[TerminSet]
+    ([PatientId]);
 GO
 
--- Creating non-clustered index for FOREIGN KEY 'FK_UserTerminBestätigung_Termin'
-CREATE INDEX [IX_FK_UserTerminBestätigung_Termin]
-ON [dbo].[UserTerminBestätigung]
-    ([bestätigt_Id]);
-GO
-
--- Creating foreign key on [wirdgewählt_Id] in table 'UserBehandlung'
-ALTER TABLE [dbo].[UserBehandlung]
-ADD CONSTRAINT [FK_UserBehandlung_User]
-    FOREIGN KEY ([wirdgewählt_Id])
-    REFERENCES [dbo].[UserSet]
-        ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-GO
-
--- Creating foreign key on [wähltBehandlung_Id] in table 'UserBehandlung'
-ALTER TABLE [dbo].[UserBehandlung]
-ADD CONSTRAINT [FK_UserBehandlung_Behandlung]
-    FOREIGN KEY ([wähltBehandlung_Id])
+-- Creating foreign key on [BehandlungId] in table 'TerminSet'
+ALTER TABLE [dbo].[TerminSet]
+ADD CONSTRAINT [FK_BehandlungTermin]
+    FOREIGN KEY ([BehandlungId])
     REFERENCES [dbo].[BehandlungSet]
         ([Id])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating non-clustered index for FOREIGN KEY 'FK_UserBehandlung_Behandlung'
-CREATE INDEX [IX_FK_UserBehandlung_Behandlung]
-ON [dbo].[UserBehandlung]
-    ([wähltBehandlung_Id]);
+-- Creating non-clustered index for FOREIGN KEY 'FK_BehandlungTermin'
+CREATE INDEX [IX_FK_BehandlungTermin]
+ON [dbo].[TerminSet]
+    ([BehandlungId]);
 GO
 
--- Creating foreign key on [wirdgewählt_Id] in table 'UserTermin'
-ALTER TABLE [dbo].[UserTermin]
-ADD CONSTRAINT [FK_UserTermin_User]
-    FOREIGN KEY ([wirdgewählt_Id])
+-- Creating foreign key on [ZeitslotId] in table 'TerminSet'
+ALTER TABLE [dbo].[TerminSet]
+ADD CONSTRAINT [FK_TerminZeitslot]
+    FOREIGN KEY ([ZeitslotId])
+    REFERENCES [dbo].[ZeitslotSet]
+        ([Id])
+    ON DELETE NO ACTION ON UPDATE NO ACTION;
+GO
+
+-- Creating non-clustered index for FOREIGN KEY 'FK_TerminZeitslot'
+CREATE INDEX [IX_FK_TerminZeitslot]
+ON [dbo].[TerminSet]
+    ([ZeitslotId]);
+GO
+
+-- Creating foreign key on [verfügbareÄrzte_Id] in table 'ArztZeitslot'
+ALTER TABLE [dbo].[ArztZeitslot]
+ADD CONSTRAINT [FK_ArztZeitslot_User]
+    FOREIGN KEY ([verfügbareÄrzte_Id])
     REFERENCES [dbo].[UserSet]
         ([Id])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating foreign key on [wähltTermin_Id] in table 'UserTermin'
-ALTER TABLE [dbo].[UserTermin]
-ADD CONSTRAINT [FK_UserTermin_Termin]
-    FOREIGN KEY ([wähltTermin_Id])
-    REFERENCES [dbo].[TerminSet]
+-- Creating foreign key on [ZeitslotArzt_Id] in table 'ArztZeitslot'
+ALTER TABLE [dbo].[ArztZeitslot]
+ADD CONSTRAINT [FK_ArztZeitslot_Zeitslot]
+    FOREIGN KEY ([ZeitslotArzt_Id])
+    REFERENCES [dbo].[ZeitslotSet]
         ([Id])
     ON DELETE NO ACTION ON UPDATE NO ACTION;
 GO
 
--- Creating non-clustered index for FOREIGN KEY 'FK_UserTermin_Termin'
-CREATE INDEX [IX_FK_UserTermin_Termin]
-ON [dbo].[UserTermin]
-    ([wähltTermin_Id]);
-GO
-
--- Creating foreign key on [CUD_T_Id] in table 'UserTermin1'
-ALTER TABLE [dbo].[UserTermin1]
-ADD CONSTRAINT [FK_UserTermin1_User]
-    FOREIGN KEY ([CUD_T_Id])
-    REFERENCES [dbo].[UserSet]
-        ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-GO
-
--- Creating foreign key on [CUDT_Id] in table 'UserTermin1'
-ALTER TABLE [dbo].[UserTermin1]
-ADD CONSTRAINT [FK_UserTermin1_Termin]
-    FOREIGN KEY ([CUDT_Id])
-    REFERENCES [dbo].[TerminSet]
-        ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-GO
-
--- Creating non-clustered index for FOREIGN KEY 'FK_UserTermin1_Termin'
-CREATE INDEX [IX_FK_UserTermin1_Termin]
-ON [dbo].[UserTermin1]
-    ([CUDT_Id]);
-GO
-
--- Creating foreign key on [CUD_B_Id] in table 'UserBehandlung1'
-ALTER TABLE [dbo].[UserBehandlung1]
-ADD CONSTRAINT [FK_UserBehandlung1_User]
-    FOREIGN KEY ([CUD_B_Id])
-    REFERENCES [dbo].[UserSet]
-        ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-GO
-
--- Creating foreign key on [CUDB_Id] in table 'UserBehandlung1'
-ALTER TABLE [dbo].[UserBehandlung1]
-ADD CONSTRAINT [FK_UserBehandlung1_Behandlung]
-    FOREIGN KEY ([CUDB_Id])
-    REFERENCES [dbo].[BehandlungSet]
-        ([Id])
-    ON DELETE NO ACTION ON UPDATE NO ACTION;
-GO
-
--- Creating non-clustered index for FOREIGN KEY 'FK_UserBehandlung1_Behandlung'
-CREATE INDEX [IX_FK_UserBehandlung1_Behandlung]
-ON [dbo].[UserBehandlung1]
-    ([CUDB_Id]);
+-- Creating non-clustered index for FOREIGN KEY 'FK_ArztZeitslot_Zeitslot'
+CREATE INDEX [IX_FK_ArztZeitslot_Zeitslot]
+ON [dbo].[ArztZeitslot]
+    ([ZeitslotArzt_Id]);
 GO
 
 -- --------------------------------------------------
