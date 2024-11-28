7179e5491270c3148626bcebd6dcae7b507f7a9e
mab6887 <mab6887@thi.de>
2024-11-28 23:48:03 +0100

    wichtige Namensanpassungen

--- TerminvereinbarungApplication/Program.cs
+++ TerminvereinbarungApplication/Program.cs
@@ -22,210 +22,269 @@ namespace TerminvereinbarungApplication
 
             //Instanziierung der Behandlungen
 
-            Behandlung ultraschall = new Behandlung();
-            ultraschall.Behandlungart = "Ultraschall";
-            ultraschall.Behandlungsdauer = new TimeSpan(0, 30, 0);
+            Behandlung ultraschall = new Behandlung
+            {
+                Behandlungart = "Ultraschall",
+                Behandlungsdauer = new TimeSpan(0, 30, 0)
+            };
 
-            Behandlung impfung = new Behandlung();
-            impfung.Behandlungart = "Impfung";
-            impfung.Behandlungsdauer = new TimeSpan(0, 15, 0);
+            Behandlung impfung = new Behandlung
+            {
+                Behandlungart = "Impfung",
+                Behandlungsdauer = new TimeSpan(0, 15, 0)
+            };
 
-            Behandlung blutabnahme = new Behandlung();
-            blutabnahme.Behandlungart = "Blutabnahme";
-            blutabnahme.Behandlungsdauer = new TimeSpan(0, 10, 0);
+            Behandlung blutabnahme = new Behandlung
+            {
+                Behandlungart = "Blutabnahme",
+                Behandlungsdauer = new TimeSpan(0, 10, 0)
+            };
 
-            Behandlung röntgen = new Behandlung();
-            röntgen.Behandlungart = "Röntgen";
-            röntgen.Behandlungsdauer = new TimeSpan(0, 20, 0);
+            Behandlung röntgen = new Behandlung
+            {
+                Behandlungart = "Röntgen",
+                Behandlungsdauer = new TimeSpan(0, 20, 0)
+            };
 
-            Behandlung diagnostischesGespräch = new Behandlung();
-            diagnostischesGespräch.Behandlungart = "Diagnostisches Gespräch";
-            diagnostischesGespräch.Behandlungsdauer = new TimeSpan(0, 30, 0);
+            Behandlung diagnostischesGespräch = new Behandlung
+            {
+                Behandlungart = "Diagnostisches Gespräch",
+                Behandlungsdauer = new TimeSpan(0, 30, 0)
+            };
 
-            Behandlung akupunktur = new Behandlung();
-            akupunktur.Behandlungart = "Akupunktur";
-            akupunktur.Behandlungsdauer = new TimeSpan(0, 45, 0);
+            Behandlung akupunktur = new Behandlung
+            {
+                Behandlungart = "Akupunktur",
+                Behandlungsdauer = new TimeSpan(0, 45, 0)
+            };
 
-            Behandlung verbinden = new Behandlung();
-            verbinden.Behandlungart = "Verbinden";
-            verbinden.Behandlungsdauer = new TimeSpan(0, 15, 0);
+            Behandlung verbinden = new Behandlung
+            {
+                Behandlungart = "Verbinden",
+                Behandlungsdauer = new TimeSpan(0, 15, 0)
+            };
 
-            Behandlung elektrokardiogramm = new Behandlung();
-            elektrokardiogramm.Behandlungart = "Elektrokardiogramm";
-            elektrokardiogramm.Behandlungsdauer = new TimeSpan(0, 20, 0);
+            Behandlung elektrokardiogramm = new Behandlung
+            {
+                Behandlungart = "Elektrokardiogramm",
+                Behandlungsdauer = new TimeSpan(0, 20, 0)
+            };
 
-            Behandlung opration = new Behandlung();
-            opration.Behandlungart = "Operation";
-            opration.Behandlungsdauer = new TimeSpan(1, 0, 0);
+            Behandlung opration = new Behandlung
+            {
+                Behandlungart = "Operation",
+                Behandlungsdauer = new TimeSpan(1, 0, 0)
+            };
 
 
             //Instanziierung der UserÄrzte
 
-            User drSchlingenbein = new User();
-            drSchlingenbein.Vorname = "Jens";
-            drSchlingenbein.Nachname = "Dr. Schlingenbein";
-            drSchlingenbein.Geburtsdatum = "03.10.1970";
-            drSchlingenbein.Telefon = "0176 1737639";
-            drSchlingenbein.EMail = "jens.schlingenbein@gmail.com";
-            drSchlingenbein.Arzt = true;
-            drSchlingenbein.Admin = false;
-            drSchlingenbein.Passwort = "Sonne1";
+            User drSchlingenbein = new User
+            {
+                Vorname = "Jens",
+                Nachname = "Dr. Schlingenbein",
+                Geburtsdatum = "03.10.1970",
+                Telefon = "0176 1737639",
+                EMail = "jens.schlingenbein@gmail.com",
+                Arzt = true,
+                Admin = false,
+                Passwort = "Sonne1"
+            };
 
-            User drLeiserbach = new User();
-            drLeiserbach.Vorname = "Carola";
-            drLeiserbach.Nachname = "Dr. Leiserbach";
-            drLeiserbach.Geburtsdatum = "12.12.1980";
-            drLeiserbach.Telefon = "0146 1737639";
-            drLeiserbach.EMail = "crola.leiserbach@gmail.com";
-            drLeiserbach.Arzt = true;
-            drLeiserbach.Admin = false;
-            drLeiserbach.Passwort = "Susanne#";
+            User drLeiserbach = new User
+            {
+                Vorname = "Carola",
+                Nachname = "Dr. Leiserbach",
+                Geburtsdatum = "12.12.1980",
+                Telefon = "0146 1737639",
+                EMail = "crola.leiserbach@gmail.com",
+                Arzt = true,
+                Admin = false,
+                Passwort = "Susanne#"
+            };
 
-            User drFreud = new User();
-            drFreud.Vorname = "Sigmund";
-            drFreud.Nachname = "Dr. Freud";
-            drFreud.Geburtsdatum = "06.05.1856";
-            drFreud.Telefon = "0176 5027639";
-            drFreud.EMail = "sigmund.freud@gmail.com";
-            drFreud.Arzt = true;
-            drFreud.Admin = false;
-            drFreud.Passwort = "Psycho1";
+            User drFreud = new User
+            {
+                Vorname = "Sigmund",
+                Nachname = "Dr. Freud",
+                Geburtsdatum = "06.05.1856",
+                Telefon = "0176 5027639",
+                EMail = "sigmund.freud@gmail.com",
+                Arzt = true,
+                Admin = false,
+                Passwort = "hübscheMütter"
+            };
 
-            User drSchönberger = new User();
-            drSchönberger.Vorname = "Thomas";
+            User drSchönberger = new User
+            {
+                Vorname = "Thomas",
+                Nachname = "Dr. Schönberger",
+                Geburtsdatum = "03.10.1975",
+                Telefon = "0156 1734269",
+                EMail = "thomas.schoenberger@gmail.com",
+                Arzt = true,
+                Admin = false,
+                Passwort = "SchönergroßerBergg"
+            };
 
-            drSchönberger.Nachname = "Dr. Schönberger";
-            drSchönberger.Geburtsdatum = "03.10.1975";
-            drSchönberger.Telefon = "0156 1734269";
-            drSchönberger.EMail = "thomas.schoenberger@gmail.com";
-            drSchönberger.Arzt = true;
-            drSchönberger.Admin = false;
-            drSchönberger.Passwort = "SchönergroßerBergg";
+            User drMaxwell = new User
+            {
+                Vorname = "Hamilton",
+                Nachname = "Dr. Maxwell",
+                Geburtsdatum = "07.08.1985",
+                Telefon = "0122 1749639",
+                EMail = "hamilton.maxwell@gmail.com",
+                Arzt = true,
+                Admin = false,
+                Passwort = "CircusMa#imus"
+            };
 
-            User drMaxwell = new User();
-            drMaxwell.Vorname = "Hamilton";
-            drMaxwell.Nachname = "Dr. Maxwell";
-            drMaxwell.Geburtsdatum = "07.08.1985";
-            drMaxwell.Telefon = "0122 1749639";
-            drMaxwell.EMail = "hamilton.maxwell@gmail.com";
-            drMaxwell.Arzt = true;
-            drMaxwell.Admin = false;
-            drMaxwell.Passwort = "CircusMa#imus";
+            User drKocht = new User
+            {
+                Vorname = "Robert",
+                Nachname = "Dr. Kocht",
+                Geburtsdatum = "03.10.1968",
+                Telefon = "0176 6589339",
+                EMail = "robert.kocht@gmail.com",
+                Arzt = true,
+                Admin = false,
+                Passwort = "Kochtopf1"
+            };
 
-            User drKocht = new User();
-            drKocht.Vorname = "Robert";
-            drKocht.Nachname = "Dr. Kocht";
-            drKocht.Geburtsdatum = "03.10.1968";
-            drKocht.Telefon = "0176 6589339";
-            drKocht.EMail = "robert.kocht@gmail.com";
-            drKocht.Arzt = true;
-            drKocht.Admin = false;
-            drKocht.Passwort = "Kochtopf1";
-
-            User drPlankton = new User();
-            drPlankton.Vorname = "Max";
-            drPlankton.Nachname = "Dr. Plankton";
-            drPlankton.Geburtsdatum = "23.06.1968";
-            drPlankton.Telefon = "01520 174t62339";
-            drPlankton.EMail = "max.plankton@gmail.com";
-            drPlankton.Arzt = true;
-            drPlankton.Admin = false;
-            drPlankton.Passwort = "Sp0ng3B00B";
+            User drPlankton = new User
+            {
+                Vorname = "Max",
+                Nachname = "Dr. Plankton",
+                Geburtsdatum = "23.06.1968",
+                Telefon = "01520 174t62339",
+                EMail = "max.plankton@gmail.com",
+                Arzt = true,
+                Admin = false,
+                Passwort = "Sp0ng3B00B"
+            };
 
 
             //Instanziierung der UserAdmin
 
-            User polo = new User();
+            User polo = new User
+            {
+                Vorname = "Marco",
+                Nachname = "Polo",
+                Geburtsdatum = "05.04.2002",
+                Telefon = "0176 12345678",
+                EMail = "mab6887@thi.de",
+                Arzt = false,
+                Admin = true,
+                Passwort = "1234"
+            };
 
-            polo.Vorname = "Marco";
-            polo.Nachname = "Polo";
-            polo.Geburtsdatum = "05.04.2002";
-            polo.Telefon = "0176 12345678";
-            polo.EMail = "mab6887@thi.de";
-            polo.Arzt = false;
-            polo.Admin = true;
-            polo.Passwort = "1234";
-            
 
             //Instanziierung der UserPatienten
 
-            User lovelace = new User();
-            lovelace.Vorname = "Ada";
-            lovelace.Nachname = "Lovelace";
-            lovelace.Geburtsdatum = "10.12.1815";
-            lovelace.Telefon = "015 35792493";
-            lovelace.EMail = "ada.lovelace@icloud.com";
-            lovelace.Krankenkasse = "AOK";
-            lovelace.Arzt = false;
-            lovelace.Admin = false;
-            lovelace.Passwort = "AdamandEva";
+            User lovelace = new User
+            {
+                Vorname = "Ada",
+                Nachname = "Lovelace",
+                Geburtsdatum = "10.12.1815",
+                Telefon = "015 35792493",
+                EMail = "ada.lovelace@icloud.com",
+                Krankenkasse = "AOK",
+                Arzt = false,
+                Admin = false,
+                Passwort = "AdamandEva"
+            };
 
-            User bubble = new User();
-            bubble.Vorname = "Edwin";
-            bubble.Nachname = "Bubble";
-            bubble.Geburtsdatum = "10.12.1815";
-            bubble.Telefon = "015 35792493";
-            bubble.EMail = "edwin.bubble@gmx.de";
-            bubble.Krankenkasse = "DAK";
-            bubble.Arzt = false;
-            bubble.Admin = false;
-            bubble.Passwort = "Bubblegum";
+            User bubble = new User
+            {
+                Vorname = "Edwin",
+                Nachname = "Bubble",
+                Geburtsdatum = "10.12.1815",
+                Telefon = "015 35792493",
+                EMail = "edwin.bubble@gmx.de",
+                Krankenkasse = "DAK",
+                Arzt = false,
+                Admin = false,
+                Passwort = "Bubblegum"
+            };
 
-            User deckenbauer = new User();
-            deckenbauer.Vorname = "Franz";
-            deckenbauer.Nachname = "Deckenbauer";
-            deckenbauer.Geburtsdatum = "10.12.1960";
-            deckenbauer.Telefon = "0174 8402ß4582";
-            deckenbauer.EMail = "franz.deckenbauer@gmail.com";
-            deckenbauer.Krankenkasse = "TK";
-            deckenbauer.Arzt = false;
-            deckenbauer.Admin = false;
-            deckenbauer.Passwort = "Dachdecker";
+            User deckenbauer = new User
+            {
+                Vorname = "Franz",
+                Nachname = "Deckenbauer",
+                Geburtsdatum = "10.12.1960",
+                Telefon = "0174 8402ß4582",
+                EMail = "franz.deckenbauer@gmail.com",
+                Krankenkasse = "TK",
+                Arzt = false,
+                Admin = false,
+                Passwort = "Dachdecker"
+            };
 
-            User hopper = new User();
-            hopper.Vorname = "Grace";
-            hopper.Nachname = "Hopper";
-            hopper.Geburtsdatum = "09.12.1906";
-            hopper.Telefon = "0176 12348648";
-            hopper.EMail = "grace.hopper@hotmail.com";
-            hopper.Krankenkasse = "BKK";
-            hopper.Arzt = false;
-            hopper.Admin = false;
-            hopper.Passwort = "GrasHopper";
+            User hopper = new User
+            {
+                Vorname = "Grace",
+                Nachname = "Hopper",
+                Geburtsdatum = "09.12.1906",
+                Telefon = "0176 12348648",
+                EMail = "grace.hopper@hotmail.com",
+                Krankenkasse = "BKK",
+                Arzt = false,
+                Admin = false,
+                Passwort = "GrasHopper"
+            };
 
-            User hamilton = new User();
-            hamilton.Vorname = "Margaret";
-            hamilton.Nachname = "Hamilton";
-            hamilton.Geburtsdatum = "17.08.1936";
-            hamilton.Telefon = "0176 53748648";
-            hamilton.EMail = "margarez.hamilton.web.de";
-            hamilton.Krankenkasse = "Barmer";
-            hamilton.Arzt = false;
-            hamilton.Admin = false;
-            hamilton.Passwort = "HamiltonsRule";
+            User dingle = new User
+            {
+                Vorname = "Quandale",
+                Nachname = "Dingle",
+                Geburtsdatum = "17.08.1936",
+                Telefon = "0176 53748648",
+                EMail = "quandale.dingle@web.de",
+                Krankenkasse = "Barmer",
+                Arzt = false,
+                Admin = false,
+                Passwort = "goofyahh"
+            };
 
-            User perlman = new User();
-            perlman.Vorname = "Radia";
-            perlman.Nachname = "Perlman";
-            perlman.Geburtsdatum = "01.01.1951";
-            perlman.Telefon = "0176 12348648";
-            perlman.EMail = "radia.perlman@gmail.com";
-            perlman.Krankenkasse = "IKK";
-            perlman.Arzt = false;
-            perlman.Admin = false;
-            perlman.Passwort = "PearlBracelet";
+            User pork = new User
+            {
+                Vorname = "John",
+                Nachname = "Pork",
+                Geburtsdatum = "01.04.1951",
+                Telefon = "0176 12348648",
+                EMail = "john.pork@gmail.com",
+                Krankenkasse = "IKK",
+                Arzt = false,
+                Admin = false,
+                Passwort = "SchinkenzumFrühstück"
+            };
+
+            User wickson = new User
+            {
+                Vorname = "Willhard",
+                Nachname = "Wickson",
+                Geburtsdatum = "20.04.1947",
+                Telefon = "0176 1239473",
+                EMail = "willhard.wickson@gmail.com",
+                Krankenkasse = "TK",
+                Arzt = false,
+                Admin = false,
+                Passwort = "mArceldAvis"
+            };
 
 
             //Instanziierung der Zeitslots
 
 
             //Instanziierung eines Tages möglicherweise später mit Mittagspause und 30 min Slots weitere for schleife für woche, weitere for schleife für jahr
-           
+
             for (int i = 0; i < 8; i++)
             {
-                Zeitslot slot = new Zeitslot();
-                slot.Startzeitpunkt = new DateTime(2025, 1, 1, 8 + i, 0, 0);
-                slot.Dauer = new TimeSpan(1, 0, 0);
+                Zeitslot slot = new Zeitslot
+                {
+                    Startzeitpunkt = new DateTime(2025, 1, 1, 8 + i, 0, 0),
+                    Dauer = new TimeSpan(1, 0, 0)
+                };
             }
             
 
